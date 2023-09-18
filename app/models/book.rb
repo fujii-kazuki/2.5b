acc6453f38fb6fb3b40dfc53addb8e03f7d641d6
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-18 02:47:57 +0000

    [Add] Bookモデルに6~2日前の各投稿を取得するスコープを定義

--- app/models/book.rb
+++ app/models/book.rb
@@ -27,6 +27,12 @@ class Book < ApplicationRecord
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   # 昨日
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  # 2~6日前
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
   # 今週
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   # 先週
